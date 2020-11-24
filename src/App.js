12c5d6c56fb14954eac1f56a4b8dc9185afd54a6
cassain2019 <cashif.hussain@gmail.com>
2020-11-24 11:00:24 +0500

    Covid Tracker via CP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,107 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import NavBar from './Components/NavBar'
-import InfoPanel from './Components/InfoPanel'
+import {  MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core';
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from './Table';
+import {sortData, sortedData} from './util';
+import LineGraph from './LineGraph';
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  // STATE = How to write a variable in REACT
+  // USEEFFECT = Runs a piece of code based on given condition
+  // async = Send a request, wait for it, do something with info.
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  const [mapCenter, setMapCenter] = useState({ lat:34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  
+  const [mapCountries, setMapCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data);
+    })
+  },[])
+
+
+  useEffect(()=> {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+          name: country.country,
+          value: country.countryInfo.iso2
+        }));
+        const sortedData = sortData(data);
+        setTableData(sortedData);
+        setMapCountries(data);
+        setCountries(countries);        
+      });
+    }
+    getCountriesData();
+}, []);
+
+const onCountryChange = async (event) => {
+  const countryCode = event.target.value;
+  setCountry(countryCode);
+  const url = countryCode === 'worldwide' 
+  ? 'https://disease.sh/v3/covid-19/all' 
+  : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+  await fetch (url)
+  .then(response => response.json())
+  .then(data => {
+    setCountry(countryCode);
+    setCountryInfo(data);
+    setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+    console.log(mapCenter);
+    setMapZoom(4);
+  })
+};
+
+console.log("Country Info >>>>",countryInfo);
+
+
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <InfoPanel></InfoPanel>
-    </div>
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+            <FormControl className="app__dropdown">
+              <Select variant="outlined" onChange={onCountryChange} value={country}>
+                  <MenuItem value="worldwide">Worldwide</MenuItem>
+                  {countries.map(country => (
+                    <MenuItem value={country.value}>{country.name}</MenuItem>
+                    ))}            
+              </Select>
+            </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}></InfoBox>
+          <InfoBox title="Recovered Cases" cases={countryInfo.todayRecovered} total={countryInfo.recovered}></InfoBox>
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} ></InfoBox>
+        </div>
+          <Map countries={mapCountries} center={mapCenter} zoom={mapZoom}/>
+        </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData}></Table>
+          <h3>Worldwide New Cases</h3>
+        </CardContent>
+        <LineGraph></LineGraph>
+
+                
+      </Card>
+  </div>
   );
 }
 
---
+++ src/util.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Numeral from 'numeral';
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+    cases: {
+        hex: "#CC1034",
+        rgb: "rgb(204, 16, 52)",
+        half_op: "rgb(204, 16, 52, 0.5)",
+        multiplier: 800,
+    },
+    recovered: {
+        hex: "#7dd71d",
+        rgb: "rgb(125, 215, 29)",
+        half_op: "rgb(125, 215, 29, 0.5)",
+        multiplier: 1200,
+    },
+    deaths: {
+        hex: "#fb4443",
+        rgb: "rgb(251, 68, 67)",
+        half_op: "rgb(251, 68, 67, 0.5)",
+        multiplier: 2000,
+    },
+};
+
+export const sortData = (data) => {
+    const sortedData = [...data];
+    sortedData.sort((a,b) => {
+        if (a.cases > b.cases) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+    return sortedData;
+} 
+
+//Draw Circles on the Map
+export const showDataOnMap = (data, casesType = "cases") => (
+    data.map(country => (
+        <Circle
+        center={[country.countryInfo.lat, country.countryInfo.lng]}
+        fillOpacity={0.4}
+        color={casesTypeColors[casesType].hex}
+        fillColor={casesTypeColors[casesType].hex}
+        radius={
+            Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+        }
+        >
+
+        </Circle>
+    ))
+)
